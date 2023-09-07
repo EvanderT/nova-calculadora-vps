a787d2edcba7e5ac898c7f9325d7c7f5aaf4bae1
EvanderT <evanderdiasteixeira@gmail.com>
2023-09-07 11:56:28 +0100

    Preço com base no tipo de armazenamento

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-
     //valores de custos
     let totalPagamento = 0;
     let coresValue = 0;
@@ -8,6 +7,13 @@ document.addEventListener('DOMContentLoaded', function () {
     let bandValue = 0;
     let ipValue = 0;
     let totalPagamentoInfo = document.getElementById('custo_total');
+
+    //Multiplicadores
+    let coreMultiplier = 1550;
+    let ramMultiplier = 895;
+    let storageMultiplier = 25;
+    let bandgeMultiplier = 10;
+    let ipMultiplier = 2000;
 
     //capturar todos os conjuntos de propriedade da calculadora
     let listaPropriedades = document.querySelectorAll('.propriedade');
@@ -26,10 +32,6 @@ document.addEventListener('DOMContentLoaded', function () {
             actualizarCustoPagamento(coresValue, ramValue);
         });
     });
-
-
-
-
 
 
     function actualizarCalculadora(propriedadesCalculadora) {
@@ -74,23 +76,23 @@ document.addEventListener('DOMContentLoaded', function () {
         //Atribuir valores as variáveis de preços com base no id do input do tipo range
         switch (elemento.id) {
             case 'cores_value':
-                coresValue = parseInt(elemento.value) * 1550;
+                coresValue = parseInt(elemento.value) * coreMultiplier;
                 break;
 
             case 'ram_value':
-                ramValue = parseInt(elemento.value)*895;
+                ramValue = parseInt(elemento.value)*ramMultiplier;
                 break;
 
             case 'storage_value':
-                storageValue = parseInt(elemento.value)*25;
+                storageValue = parseInt(elemento.value)*storageMultiplier;
                 break;
 
             case 'band_value':
-                bandValue = parseInt(elemento.value)*10;
+                bandValue = parseInt(elemento.value)*bandgeMultiplier;
                 break;
 
             case 'ip_value':
-                ipValue = parseInt(elemento.value)*(2000);
+                ipValue = parseInt(elemento.value)*ipMultiplier;
                 break;
         }
 
@@ -99,4 +101,32 @@ document.addEventListener('DOMContentLoaded', function () {
     actualizarCalculadora(listaPropriedades);
 
 
+
+    // CONTROLE DA TROCA DE ARMAZENAMENTO  SSD & HDD
+    let containetTipoArmazenamento = document.querySelector('.storage_group');
+    let tiposArmazenamentos = document.querySelectorAll('.storage_type');
+    let storageSSD = true;
+
+    tiposArmazenamentos.forEach(function(tipoArmazenamento){
+        tipoArmazenamento.addEventListener('click',alterarTipoDeArmazenamento);
+    })
+
+    function alterarTipoDeArmazenamento(){
+        tiposArmazenamentos.forEach(function(tipoArmazenamento){
+            tipoArmazenamento.classList.toggle('active');
+            
+        });
+        containetTipoArmazenamento.classList.toggle('active');
+
+        if(storageSSD){
+            storageSSD = false;
+            storageMultiplier= 60;
+        }
+        else{
+            storageSSD = true;
+            storageMultiplier= 25;
+        }
+
+        actualizarCalculadora(listaPropriedades);
+    }
 });
